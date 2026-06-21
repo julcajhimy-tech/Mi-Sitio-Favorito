1aa728bcda8b4204f38a35bdf1b927922b3142ef
julcajhimy-tech <julcajhimy@gmail.com>
2026-06-21 18:15:15 -0500

    FIX: Use gevent worker in start script

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ python -m scripts.init_db
 
 # Inicia el servidor de la aplicación
 echo "Iniciando la aplicación..."
-python -m gunicorn --worker-class eventlet -w 1 run:app
+python -m gunicorn --worker-class gevent -w 1 run:app
